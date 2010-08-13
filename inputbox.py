f8c379998fc1e031ff57e4e96e83f3ca761a6f1e
parse <anders.hassis@gmail.com>
2010-08-13 12:30:42 +0000

    Just need to empty the database and we're good to go

--- inputbox.py
+++ inputbox.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#
 # Title: Inputbox Module
 # Author: Timothy Downs
 # Submission date: January 23, 2002 
 # Description: A function to display an inputbox and return the inputted text string.# by Timothy Downs, inputbox written for my map editor
 # This program needs a little cleaning up
-# It ignores the shift key
+# It ignores the shift key 
 # And, for reasons of my own, this program converts "-" to "_"
 
 # A program to get user input, allowing backspace etc
