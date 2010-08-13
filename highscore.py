feaf3321448a54a4432fec278bd366b1980bbd10
JohNan <johan.nanzen@gmail.com>
2010-08-13 12:26:05 +0000

    Added english comments to highscore.py

--- highscore.py
+++ highscore.py
@@ -1,4 +1,6 @@
-﻿#
+<<<<<<< .mine
+import sqlite3
+#
 # Multimediaprogrammering i Python ST2010
 # written by Jonatan Jansson, Anders Hassis & Johan Nänzen
 # using Python 2.6.4
@@ -8,86 +10,82 @@ import sqlite3
 import pygame
 from pygame.locals import *
 
-# Klass highscore.
-# Skriver ut och uppdaterar highscores
+# Class highscore.
+# Prints and updates the highscore tabel
 
 class Highscore:
-    # Initiering av higscore
+    # Init higscore
     def __init__(self, surface):
 
-        # Skapar en sqlite-anslutning till test.db, denna skapas om den inte finns.
+        # Creates a sqlite connection to test.db, will be created if non-exist
         connection = sqlite3.connect('test.db')
         cursor = connection.cursor()        
 
-        # Skapar en tabell vid namn highscore där namn och poäng lagras.
-        # Om tabellen redan finns får man ett exception, som hanteras och stänger den öppna pekaren mot anslutningen.
+        # Creates a table where name and score are stored
         try:
             cursor.execute('CREATE TABLE highscore (id INTEGER PRIMARY KEY, name VARCHAR(50), score INTEGER)')
             cursor.close()
         except sqlite3.Error, e:
             cursor.close()
             
-        self.surface = surface      # ytan som highscore ska skrivas ut på
-        self.db = connection        # anslutningen till databasen
-        self.cur = self.db.cursor() # pekarenmot databasen
+        self.surface = surface      
+        self.db = connection        
+        self.cur = self.db.cursor() 
 
-    # Uppdaterar highscore
-    # tar emot ett namn, poäng och en font.
-    # Detta lagras sedan i databasen.
+    # Updates highscore and store in database
     def update(self, name, score, font):
         player = []
         i = 1
     
-        self.cur.execute("SELECT COUNT(*) FROM highscore" )     # Kollar antalet inlägg i databasen
+        self.cur.execute("SELECT COUNT(*) FROM highscore" )     # Check number of rows in table
         result = self.cur.fetchall()
         nrecs = result[0][0]
         
-        if nrecs > 10:              # Om det är mer än 10 inlägg
-            self.cur.execute('SELECT * FROM highscore ORDER BY score DESC LIMIT 9,1')       # Väljer det sista och kollar om det är mindre poäng än det som användaren fick.
+        if nrecs > 10:              # If 10 or more rows
+            self.cur.execute('SELECT * FROM highscore ORDER BY score DESC LIMIT 9,1')       # Compares the last row with the current score
             last = self.cur.fetchone()
             #print last
             if last[2] < score:
                 try:                
-                    self.cur.execute('UPDATE highscore SET name=?, score=? WHERE id=?', (name, score, last[0]))    # Har användaren högre poäng läggs det in på det ID som petas ut.
+                    self.cur.execute('UPDATE highscore SET name=?, score=? WHERE id=?', (name, score, last[0]))    # Update the last row with new score and name
                 except sqlite3.Error, e:
                     print "Ooops:", e.args[0]
-        else:                       # Lägg annars till det i tabellen
+        else:                       # Add a new row in table
             try:
                 self.cur.execute('INSERT INTO highscore VALUES (null, ?, ?)', (name, score))
             except sqlite3.Error, e:
                 print "Ooops:", e.args[0]
             
         try: 
-            self.cur.execute('SELECT * FROM highscore ORDER BY score DESC LIMIT 0,10')      # Välj 10 inlägg med start från första
+            self.cur.execute('SELECT * FROM highscore ORDER BY score DESC LIMIT 0,10')      # Choose the first 10 rows
         except sqlite3.Error, e:
             print "Ooops:", e.args[0]
 
-        for row in self.cur:				#Loopa genom inläggen och spara i en array för att sedan kunna skriva ut highscoren
+        for row in self.cur:				# Loop though the rows and adds to a array
             player.append(font.render(str(i) + '. ' + str(row[1]) + '  '*(8-len(row[1])) + str(row[2]), True, (255, 0, 0)))
             i += 1
             
-        return player                   # Returnera arrayen med det som ska skrivas ut
+        return player                   # Return the array
 
-    # Skriver ut highscore.
-    # tar in player som är en array med listan av highscore.
+    # Genereate and prints highscore.
     def draw(self, player, font):
         i = 20
         font2 = pygame.font.SysFont('Arial Black', 40)
-        trans = pygame.Surface((300, 350))              # Skapar en genomskinlig ruta som ligger bakom highscore
+        trans = pygame.Surface((300, 350))              # Creates transparent rectangle
         trans.fill((0,0,0))
         pygame.Surface.convert_alpha(trans)
         trans.set_alpha(128)
 
-        self.surface.blit(trans, (self.surface.get_width()/2-130,80))        # Skriv ut rektangeln                        
+        self.surface.blit(trans, (self.surface.get_width()/2-130,80))                        
         
-        gameOverImg = font2.render("Game Over", True, (255, 0, 0))                  # Skapar en Game Over text
-        highScoreImg = font.render("Name      Score", True, (255, 0, 0))            # Skapar Name Score till highscore
-        pressSpaceImg = font.render("Press space to try again", True, (255, 0, 0))  # Skapar Press space to try again som  ligger i nedre högra hörnet
+        gameOverImg = font2.render("Game Over", True, (255, 0, 0))                 
+        highScoreImg = font.render("Name      Score", True, (255, 0, 0))            
+        pressSpaceImg = font.render("Press space to try again", True, (255, 0, 0))  
         
-        for row in player:              # Loppa igenom player och skriv ut varje rad på skärmen
+        for row in player:              # Loops through player and blits every row with 20px between
             self.surface.blit(row, (self.surface.get_width()/2-100, 140+i))
-            i += 20                 # 20 pixlar mellan varje rad
+            i += 20
 
-        self.surface.blit(gameOverImg, (self.surface.get_width()/2-120, 70))                            # Skriver ut game over
-        self.surface.blit(highScoreImg, (self.surface.get_width()/2-100, 140))                          # Skriver ut highscore headern
-        self.surface.blit(pressSpaceImg, (self.surface.get_width()-250, self.surface.get_height()-30))  # Skriver ut press space again...
+        self.surface.blit(gameOverImg, (self.surface.get_width()/2-120, 70))                            
+        self.surface.blit(highScoreImg, (self.surface.get_width()/2-100, 140))                          
+        self.surface.blit(pressSpaceImg, (self.surface.get_width()-250, self.surface.get_height()-30))
